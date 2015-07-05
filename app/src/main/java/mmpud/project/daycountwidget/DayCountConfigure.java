9cb3523fd719ea5bf5de99e4fed2955692a0239e
George Lin <georgelin422@gmail.com>
2015-07-05 17:07:19 +0800

    fix on

--- app/src/main/java/mmpud/project/daycountwidget/DayCountConfigure.java
+++ app/src/main/java/mmpud/project/daycountwidget/DayCountConfigure.java
@@ -1,6 +1,5 @@
 package mmpud.project.daycountwidget;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.WallpaperManager;
@@ -11,58 +10,57 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
-import it.sephiroth.android.library.widget.AdapterView;
-import it.sephiroth.android.library.widget.HListView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import mmpud.project.daycountwidget.misc.ClickableRecyclerAdapter;
+import mmpud.project.daycountwidget.misc.RecyclerViewOnItemClickListener;
 import mmpud.project.daycountwidget.util.Utils;
 import timber.log.Timber;
 
 public class DayCountConfigure extends Activity {
 
+    @Bind(R.id.tv_date) TextView mTvDate;
+    @Bind(R.id.edt_title) EditText mEdtTitle;
+    @Bind(R.id.btn_ok) Button mBtnOK;
+    @Bind(R.id.sample_widget_bg) FrameLayout mFlSampleWidget;
+    @Bind(R.id.sample_widget) LinearLayout mLlSampleWidgetBody;
+    @Bind(R.id.sample_widget_title) TextView mTvSampleWidgetHeader;
+    @Bind(R.id.sample_widget_diffdays) TextView mTvSampleWidgetDiffDays;
+    @Bind(R.id.sample_widget_since_left) TextView mTvSampleWidgetSinceLeft;
+    @Bind(R.id.hlv_style_select_header) RecyclerView mHlvSelectHeader;
+    @Bind(R.id.hlv_style_select_body) RecyclerView mHlvSelectBody;
+
     private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     private String mStyleHeader;
     private String mStyleBody;
-
-    private TextView mTvDate;
-    private EditText mEdtTitle;
-    private Button mBtnOK;
-
-    // Sample preview widget
-    private FrameLayout mFlSampleWidget;
-    private LinearLayout mLlSampleWidgetBody;
-    private TextView mTvSampleWidgetHeader;
-    private TextView mTvSampleWidgetDiffDays;
-    private TextView mTvSampleWidgetSinceLeft;
-
-    // Style selection list (header)
-    private HListView mHlvSelectHeader;
-    private SelectStyleAdapter mHeaderAdapter;
-    private List<String> mHeaderStyleList;
-
-    // Style selection list (body)
-    private HListView mHlvSelectBody;
-    private SelectStyleAdapter mBodyAdapter;
-    private List<String> mBodyStyleList;
-
+    private MyAdapter mHeaderAdapter;
+    private MyAdapter mBAdapter;
     private DatePickerDialog mDatePickerDialog;
 
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
@@ -71,55 +69,56 @@ public class DayCountConfigure extends Activity {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.tv_date:
-                    if (mDatePickerDialog != null) {
-                        mDatePickerDialog.show();
-                    }
-                    break;
-                case R.id.btn_ok:
-                    // Save information: 1. YYYY-MM-DD
-                    //		    		 2. title
-                    //					 3. header and body style
-                    // to shared preferences according to the appWidgetId
-                    SharedPreferences.Editor prefs = context.getSharedPreferences(Utils.PREFS_NAME, 0).edit();
-                    String targetDate = mTvDate.getText().toString();
-                    prefs.putString(Utils.KEY_TARGET_DATE + mAppWidgetId, targetDate);
-                    prefs.putString(Utils.KEY_TITLE + mAppWidgetId, mEdtTitle.getText().toString());
-                    prefs.putString(Utils.KEY_STYLE_HEADER + mAppWidgetId, mStyleHeader);
-                    prefs.putString(Utils.KEY_STYLE_BODY + mAppWidgetId, mStyleBody);
-                    prefs.commit();
+            case R.id.tv_date:
+                if (mDatePickerDialog != null) {
+                    mDatePickerDialog.show();
+                }
+                break;
+            case R.id.btn_ok:
+                // Save information: 1. YYYY-MM-DD
+                //		    		 2. title
+                //					 3. header and body style
+                // to shared preferences according to the appWidgetId
+                SharedPreferences.Editor prefs = context.getSharedPreferences(Utils.PREFS_NAME,
+                    0).edit();
+                String targetDate = mTvDate.getText().toString();
+                prefs.putString(Utils.KEY_TARGET_DATE + mAppWidgetId, targetDate);
+                prefs.putString(Utils.KEY_TITLE + mAppWidgetId, mEdtTitle.getText().toString());
+                prefs.putString(Utils.KEY_STYLE_HEADER + mAppWidgetId, mStyleHeader);
+                prefs.putString(Utils.KEY_STYLE_BODY + mAppWidgetId, mStyleBody);
+                prefs.commit();
 
-                    RemoteViews views = DayCountWidget.buildRemoteViews(context, mAppWidgetId);
-                    // Push widget update to surface
-                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-                    appWidgetManager.updateAppWidget(mAppWidgetId, views);
-                    Timber.d("The widget [" + mAppWidgetId + "] is updated.");
-                    // Make sure we pass back the original appWidgetId
-                    Intent resultValue = new Intent();
-                    resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-                    setResult(RESULT_OK, resultValue);
-                    finish();
-                    break;
+                RemoteViews views = DayCountWidget.buildRemoteViews(context, mAppWidgetId);
+                // Push widget update to surface
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                appWidgetManager.updateAppWidget(mAppWidgetId, views);
+                Timber.d("The widget [" + mAppWidgetId + "] is updated.");
+                // Make sure we pass back the original appWidgetId
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                setResult(RESULT_OK, resultValue);
+                finish();
+                break;
             }
         }
     };
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setResult(RESULT_CANCELED); // Set the result to CANCELED.
-                                    // This will cause the widget host to cancel out of the
-                                    // widget placement if they press the back button.
+        // This will cause the widget host to cancel out of the
+        // widget placement if they press the back button.
         setContentView(R.layout.day_count_configure_layout);
+        ButterKnife.bind(this);
         // Get the widget id from the intent.
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         if (extras != null) {
             mAppWidgetId = extras.getInt(
-                    // INVALID_APPWIDGET_ID is 0
-                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+                // INVALID_APPWIDGET_ID is 0
+                AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
         }
         Timber.d("mAppWidgetId: " + mAppWidgetId);
         // If they gave us an intent without the widget id, just bail.
@@ -144,21 +143,13 @@ public class DayCountConfigure extends Activity {
         mStyleBody = prefs.getString(Utils.KEY_STYLE_BODY + mAppWidgetId, "body_black");
 
         Timber.d("(initTargetDate, initTitle, mStyleHeader, mStyleBody): "
-                + "(" + initTargetDate
-                + ", " + initTitle
-                + ", " + mStyleHeader
-                + ", " + mStyleBody + ")");
+            + "(" + initTargetDate
+            + ", " + initTitle
+            + ", " + mStyleHeader
+            + ", " + mStyleBody + ")");
 
-        mTvDate = (TextView) findViewById(R.id.tv_date);
         mTvDate.setOnClickListener(mOnClickListener);
-        mEdtTitle = (EditText) findViewById(R.id.edt_title);
-        mBtnOK = (Button) findViewById(R.id.btn_ok);
         mBtnOK.setOnClickListener(mOnClickListener);
-        mFlSampleWidget = (FrameLayout) findViewById(R.id.sample_widget_bg);
-        mTvSampleWidgetHeader = (TextView) findViewById(R.id.sample_widget_title);
-        mTvSampleWidgetDiffDays = (TextView) findViewById(R.id.sample_widget_diffdays);
-        mTvSampleWidgetSinceLeft = (TextView) findViewById(R.id.sample_widget_since_left);
-        mLlSampleWidgetBody = (LinearLayout) findViewById(R.id.sample_widget);
 
         // Set current date into datePicker
         // Set the date picker dialog
@@ -200,7 +191,7 @@ public class DayCountConfigure extends Activity {
         // Set the home screen wallpaper to the sample widget
         final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
         final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             mFlSampleWidget.setBackground(wallpaperDrawable);
         } else {
             mFlSampleWidget.setBackgroundDrawable(wallpaperDrawable);
@@ -208,46 +199,50 @@ public class DayCountConfigure extends Activity {
 
         // Initialize the header and body style of the sample widget
         int resourceIdStyleHeader = getResources().getIdentifier(mStyleHeader,
-                "drawable", "mmpud.project.daycountwidget");
+            "drawable", "mmpud.project.daycountwidget");
         Drawable dH = getResources().getDrawable(resourceIdStyleHeader);
         mTvSampleWidgetHeader.setBackground(dH);
         int resourceIdStyleBody = getResources().getIdentifier(mStyleBody,
-                "drawable", "mmpud.project.daycountwidget");
+            "drawable", "mmpud.project.daycountwidget");
         Drawable dB = getResources().getDrawable(resourceIdStyleBody);
         mLlSampleWidgetBody.setBackground(dB);
 
         // Set up the horizontal list for header style selection
-        mHlvSelectHeader = (HListView) findViewById(R.id.hlv_style_select_header);
-        mHeaderStyleList = new ArrayList<String>();
-        mHeaderStyleList = Arrays.asList(getResources().getStringArray(R.array.header_style_list));
-        mHeaderAdapter = new SelectStyleAdapter(this, R.layout.list_item_style, mHeaderStyleList);
-        mHlvSelectHeader.setAdapter(mHeaderAdapter);
-        mHlvSelectHeader.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Timber.d("set up the horizontal scroll recycler view");
+        mHeaderAdapter = new MyAdapter(
+            Arrays.asList(getResources().getStringArray(R.array.header_style_list)));
+        mHeaderAdapter.setOnItemClickListener(new RecyclerViewOnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mStyleHeader = mHeaderStyleList.get(i);
+            public void onItemClick(RecyclerView.Adapter adapter, View view, int position) {
+                mStyleHeader = mHeaderAdapter.getItem(position);
                 int resourceIdStyleHeader = getResources().getIdentifier(mStyleHeader,
-                        "drawable", "mmpud.project.daycountwidget");
-                Drawable d = getResources().getDrawable(resourceIdStyleHeader);
-                mTvSampleWidgetHeader.setBackground(d);
+                    "drawable", "mmpud.project.daycountwidget");
+                mTvSampleWidgetHeader.setBackgroundResource(resourceIdStyleHeader);
             }
         });
+        mHlvSelectHeader.setHasFixedSize(true);
+        mHlvSelectHeader.setItemAnimator(null);
+        mHlvSelectHeader.setLayoutManager(new LinearLayoutManager(this,
+            LinearLayoutManager.HORIZONTAL, false));
+        mHlvSelectHeader.setAdapter(mHeaderAdapter);
         // Set up the horizontal list for body style selection
-        mHlvSelectBody = (HListView) findViewById(R.id.hlv_style_select_body);
-        mBodyStyleList = new ArrayList<String>();
-        mBodyStyleList = Arrays.asList(getResources().getStringArray(R.array.body_style_list));
-        mBodyAdapter = new SelectStyleAdapter(this, R.layout.list_item_style, mBodyStyleList);
-        mHlvSelectBody.setAdapter(mBodyAdapter);
-        mHlvSelectBody.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mBAdapter = new MyAdapter(
+            Arrays.asList(getResources().getStringArray(R.array.body_style_list))
+        );
+        mBAdapter.setOnItemClickListener(new RecyclerViewOnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mStyleBody = mBodyStyleList.get(i);
+            public void onItemClick(RecyclerView.Adapter adapter, View view, int position) {
+                mStyleBody = mBAdapter.getItem(position);
                 int resourceIdStyleHeader = getResources().getIdentifier(mStyleBody,
-                        "drawable", "mmpud.project.daycountwidget");
-                Drawable d = getResources().getDrawable(resourceIdStyleHeader);
-                mLlSampleWidgetBody.setBackground(d);
+                    "drawable", "mmpud.project.daycountwidget");
+                mLlSampleWidgetBody.setBackgroundResource(resourceIdStyleHeader);
             }
         });
+        mHlvSelectBody.setHasFixedSize(true);
+        mHlvSelectBody.setItemAnimator(null);
+        mHlvSelectBody.setLayoutManager(new LinearLayoutManager(this,
+            LinearLayoutManager.HORIZONTAL, false));
+        mHlvSelectBody.setAdapter(mBAdapter);
     }
 
     @Override
@@ -280,6 +275,53 @@ public class DayCountConfigure extends Activity {
             mTvSampleWidgetDiffDays.setText(Integer.toString(diffDays));
             mTvSampleWidgetSinceLeft.setText(getResources().getText(R.string.days_since));
         }
+    }
+
+    public static class MyAdapter extends ClickableRecyclerAdapter<MyViewHolder> {
+
+        private final List<String> mItems;
+
+        private LayoutInflater mInflater;
+
+        public MyAdapter(List<String> mItems) {
+            this.mItems = mItems;
+        }
+
+        public String getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public MyViewHolder onCreateClickableViewHolder(ViewGroup parent, int viewType) {
+            if (mInflater == null) {
+                mInflater = LayoutInflater.from(parent.getContext());
+            }
+            return new MyViewHolder(mInflater.inflate(R.layout.style_list_item, parent, false));
+        }
+
+        @Override public void onBindViewHolder(MyViewHolder holder, int position) {
+            Context context = holder.itemView.getContext();
+            String strStyle = mItems.get(position);
+            int resourceIDStyle = context.getResources()
+                .getIdentifier(strStyle + "_config", "drawable", "mmpud.project.daycountwidget");
+            Timber.d("on bind resource name %s resource id %d", strStyle, resourceIDStyle);
+            Picasso.with(context)
+                .load(resourceIDStyle)
+                .into((ImageView) holder.itemView);
+        }
+
+        @Override public int getItemCount() {
+            return mItems.size();
+        }
+
+    }
+
+    public static class MyViewHolder extends ClickableRecyclerAdapter.ClickableViewHolder {
+
+        public MyViewHolder(View view) {
+            super(view);
+        }
+
     }
 
 }
